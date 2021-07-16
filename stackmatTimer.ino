2d95c62e190337ea7300e820680a9abc549ab41a
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-07-16 19:57:48 -0400

    new code

--- stackmatTimer.ino
+++ stackmatTimer.ino
@@ -4,34 +4,28 @@
 #define EN 2
 #define RW 1
 #define RS 0
-#define D4 4 
+#define D4 4
 #define D5 5
 #define D6 6
 #define D7 7
 #define BL 3
 #define I2C_ADDRESS 0x27
 
-LiquidCrystal_I2C lcd(I2C_ADDRESS, EN, RW, RS, D4, D5, D6, D7, BL, POSITIVE);
-
 #define B1 2
 #define B2 3
 #define RESET 4
 
-// timing values
-bool timerReady = false;
-bool timerNotRunning = true;
-
-// times
-unsigned long start = 0;
-unsigned long done = 0;
-float endTime = 0.00;
-float oldTime = 0.00;
-unsigned long timeHeld = 0;
-
-// led pins
 #define RED_PIN 5
 #define GREEN_PIN 6
 #define BLUE_PIN 7
+enum Color {RED, BLUE, GREEN};
+
+LiquidCrystal_I2C lcd(I2C_ADDRESS, EN, RW, RS, D4, D5, D6, D7, BL, POSITIVE);
+bool timerReady = false;
+bool timerRunning = false;
+
+unsigned long start = 0;
+unsigned long timeHeld = 0;
 
 void setup() {
   pinMode(B1, INPUT);
@@ -43,38 +37,50 @@ void setup() {
   pinMode(BLUE_PIN, OUTPUT);
 
   lcd.begin(16, 2);
+  Serial.begin(9600);
 
   // default settings for lights and time:
   lcd.setCursor(5, 0);
   lcd.print("0.00");
-  digitalWrite(RED_PIN, HIGH);
+  setColor(RED);
 }
 
-void convertTime(
-    float input) { // funtion to convert time to minutes:seconds.centiseconds
+void setColor(Color color) {
+  if (color == RED)
+    digitalWrite(RED_PIN, HIGH);
+  else
+    digitalWrite(RED_PIN, LOW);
+  if (color == BLUE)
+    digitalWrite(BLUE_PIN, HIGH);
+  else
+    digitalWrite(BLUE_PIN, LOW);
+  if (color == GREEN)
+    digitalWrite(GREEN_PIN, HIGH);
+  else
+    digitalWrite(GREEN_PIN, LOW);
+}
 
-  if (input > 600) {
-
+void convertTime(float time) { // funtion to convert time to minutes:seconds.centiseconds
+  if (time > 600) {
     lcd.print("DNF          ");
-
-  } else if (input < 60) {
+  } else if (time < 60) {
     // Serial.println(input);
-    lcd.print(input);
+    lcd.print(time);
   } else {
 
-    int tSecs = int(input); // total seconds
-    int secs = tSecs % 60;  // seconds that will be displayed
-    float centiSecs = input - tSecs;
+    int totalSeconds = (int)time;
+    int secs = totalSeconds % 60;  // seconds that will be displayed
+    float centiSecs = time - totalSeconds;
     float secondPart =
         secs +
         centiSecs; // includes both seconds and centiseconds for easier display
-    int mins = (tSecs - secs) / 60;
+    int mins = (totalSeconds - secs) / 60;
 
     // convert to strings in order to display
 
     String sSecondPart = String(secondPart);
     String sMins = String(mins);
-    if (tSecs % 60 < 10) {
+    if (totalSeconds % 60 < 10) {
       String output1 = sMins + ":" + "0" + sSecondPart;
 
       lcd.print(output1);
@@ -88,7 +94,6 @@ void convertTime(
 }
 
 void loop() {
-
   lcd.setCursor(5, 0);
 
   int b1Val = digitalRead(B1);
@@ -99,71 +104,44 @@ void loop() {
     lcd.clear();
     lcd.setCursor(5, 0);
     lcd.print("0.00");
+    setColor(RED);
 
-    timerNotRunning = true;
     timerReady = false;
-    oldTime = 0.00;
-
-    digitalWrite(RED_PIN, HIGH);
-    digitalWrite(GREEN_PIN, LOW);
-    digitalWrite(BLUE_PIN, LOW);
+    timerRunning = false;
   }
 
-  if (b1Val && b2Val) { // timer is about to start or is stopped
-    if (timerNotRunning) {   // timer is about to start
-      timeHeld = millis();
-      timerNotRunning = false;
-      timerReady = true;
-      digitalWrite(RED_PIN, LOW);
-      digitalWrite(GREEN_PIN, HIGH);
-      digitalWrite(BLUE_PIN, LOW);
-    }
-
-    if (!timerNotRunning && !timerReady) { // timer is stopped
-      done = millis();
-      endTime = (done / 1000.00 - start / 1000.00) + oldTime;
-
+  if (b1Val && b2Val) {
+    if (timerRunning) { //timer stopped
+      float endTime = (((float)millis()) / 1000.0) - (((float)start) / 1000.0);
       convertTime(endTime);
-
-      Serial.println(endTime);
-      timerNotRunning = true;
-      timerReady = true;
-      oldTime = endTime;
-
-      digitalWrite(GREEN_PIN, LOW);
-      digitalWrite(RED_PIN, HIGH);
-      digitalWrite(BLUE_PIN, LOW);
-      delay(1000);
-    }
-
-  } else if (b1Val || b2Val ) {
-    if (millis() - timeHeld < 150) {
       timerReady = false;
-      timerNotRunning = true;
-      digitalWrite(GREEN_PIN, LOW);
-      digitalWrite(RED_PIN, HIGH);
-      digitalWrite(BLUE_PIN, LOW);
-
+      timerRunning = false;
+      setColor(RED);
+      delay(200);
+    } else if (!timerRunning && timerReady && millis() - timeHeld > 300) {
+      setColor(GREEN);
+    } else if(!timerReady){ //hands placed on timer
+      timeHeld = millis();
+      timerRunning = false;
+      timerReady = true;
     }
 
-    else if (!timerNotRunning && timerReady ) { // time starts recording
+  } else if ((b1Val || b2Val) && millis() - timeHeld <= 300) {
+    // if someone takes off their hands 300ms after puting their hands down the timer resets
+    timerReady = false;
+    timerRunning = false;
+    setColor(RED);
+
+  } else if(!b1Val || !b2Val){
+    if(timerReady){ //timer started
       start = millis();
       timerReady = false;
+      timerRunning = true;
       delay(100);
-      digitalWrite(BLUE_PIN, HIGH);
-      digitalWrite(GREEN_PIN, LOW);
+      setColor(BLUE);
+    } else if(!timerReady && timerRunning){
+      float now = (((float)millis()) / 1000.0) - (((float)start) / 1000.0);
+      convertTime(now);
     }
-  }
-
-  while (!timerNotRunning && !timerReady) {
-    if (b1Val && b2Val ) {
-      break;
-    }
-    digitalWrite(BLUE_PIN, HIGH);
-    digitalWrite(GREEN_PIN, LOW);
-    digitalWrite(RED_PIN, LOW);
-    float currentTime = (millis() / 1000.00) - (start / 1000.00) + (oldTime);
-    convertTime(currentTime);
-    break;
-  }
+  } 
 }
