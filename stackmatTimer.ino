2bcc18d3ba51bc1fa9fe35f172c679f3ccf3c4d4
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-07-16 20:11:54 -0400

    clean up format time function

--- stackmatTimer.ino
+++ stackmatTimer.ino
@@ -27,24 +27,6 @@ bool timerRunning = false;
 unsigned long start = 0;
 unsigned long timeHeld = 0;
 
-void setup() {
-  pinMode(B1, INPUT);
-  pinMode(B2, INPUT);
-  pinMode(RESET, INPUT);
-
-  pinMode(RED_PIN, OUTPUT);
-  pinMode(GREEN_PIN, OUTPUT);
-  pinMode(BLUE_PIN, OUTPUT);
-
-  lcd.begin(16, 2);
-  Serial.begin(9600);
-
-  // default settings for lights and time:
-  lcd.setCursor(5, 0);
-  lcd.print("0.00");
-  setColor(RED);
-}
-
 void setColor(Color color) {
   if (color == RED)
     digitalWrite(RED_PIN, HIGH);
@@ -60,38 +42,42 @@ void setColor(Color color) {
     digitalWrite(GREEN_PIN, LOW);
 }
 
-void convertTime(float time) { // funtion to convert time to minutes:seconds.centiseconds
+void formatAndPrintTime(float time) { // converts time to minutes:seconds.centiseconds
   if (time > 600) {
     lcd.print("DNF          ");
   } else if (time < 60) {
-    // Serial.println(input);
     lcd.print(time);
   } else {
-
     int totalSeconds = (int)time;
-    int secs = totalSeconds % 60;  // seconds that will be displayed
-    float centiSecs = time - totalSeconds;
-    float secondPart =
-        secs +
-        centiSecs; // includes both seconds and centiseconds for easier display
-    int mins = (totalSeconds - secs) / 60;
+    int seconds = totalSeconds % 60;
+    float centiSeconds = time - totalSeconds;
+    float displayedSeconds = seconds + centiSeconds; 
+    int mins = (totalSeconds - seconds) / 60;
 
-    // convert to strings in order to display
-
-    String sSecondPart = String(secondPart);
-    String sMins = String(mins);
-    if (totalSeconds % 60 < 10) {
-      String output1 = sMins + ":" + "0" + sSecondPart;
-
-      lcd.print(output1);
-    } else {
-      String output2 =
-          sMins + ":" + sSecondPart + "      "; // time that will be displayed
-
-      lcd.print(output2);
+    if (totalSeconds % 60 < 10) 
+      lcd.print(String(mins) + ":0" + String(displayedSeconds));
+    else 
+      lcd.print(String(mins) + ":" + displayedSeconds);
     }
-  }
 }
+
+void setup() {
+  pinMode(B1, INPUT);
+  pinMode(B2, INPUT);
+  pinMode(RESET, INPUT);
+
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+
+  lcd.begin(16, 2);
+
+  // default settings for lights and time:
+  lcd.setCursor(5, 0);
+  lcd.print("0.00");
+  setColor(RED);
+}
+
 
 void loop() {
   lcd.setCursor(5, 0);
@@ -113,7 +99,7 @@ void loop() {
   if (b1Val && b2Val) {
     if (timerRunning) { //timer stopped
       float endTime = (((float)millis()) / 1000.0) - (((float)start) / 1000.0);
-      convertTime(endTime);
+      formatAndPrintTime(endTime);
       timerReady = false;
       timerRunning = false;
       setColor(RED);
@@ -141,7 +127,7 @@ void loop() {
       setColor(BLUE);
     } else if(!timerReady && timerRunning){
       float now = (((float)millis()) / 1000.0) - (((float)start) / 1000.0);
-      convertTime(now);
+      formatAndPrintTime(now);
     }
   } 
 }
